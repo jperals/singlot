0e5d401ddfaddaa216bf5bcf5fb4dba87393ef18
Joan Perals Tresserra <j.pertres@gmail.com>
2014-04-10 00:47:19 +0200

    Render the markers using angular-leaflet

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-var app = angular.module("transglobe", ['leaflet-directive'])
+var app = angular.module('transglobe', ['leaflet-directive'])
 
-    .controller("transglobeController", [ "$scope", function($scope) {
-            
+    .controller('transglobeController', [ '$scope', 'languageMarkersFactory', function($scope, languageMarkersFactory) {
+        
+        languageMarkersFactory.getLanguageMarkers().then(function(markers) {
+            $scope.markers = markers;
+        });
+
         angular.extend($scope, {
             defaults: {
                 tileLayer: 'http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png',
@@ -10,10 +14,111 @@ var app = angular.module("transglobe", ['leaflet-directive'])
                 lat: 52.5,
                 lng: 2,
                 zoom: 4
-            }
+            },
+            markers: []
         });
         
     }])
+    
+    .factory('languageMarkersFactory', [ '$http', '$q', 'languageService', function($http, $q, languageService) {
+
+        var getMarkerFromLanguage = function(language, opt_location) {
+            var markerLocation = opt_location || language.location;
+            if(typeof markerLocation !== "undefined") {
+                var languageName = languageService.getLanguageName(language);
+                var languageCode = languageService.getLanguageCode(language);
+                var languageIcon = {
+                    className: 'language-label',
+                    html: '<div class="translation-container empty" data-language="' + languageCode + '"><div contenteditable="true" class="translation language-' + languageCode + '"></div><div class="placeholder">' + languageName + '</div><a href="#" role="button">Go</a></div>',
+                    type: 'div'
+                };
+                var marker = {
+                    clickable: false,
+                    icon: languageIcon,
+                    lat: markerLocation.lat,
+                    lng: markerLocation.lon,
+                    title: 'Write something in ' + languageName
+                };
+                marker.language = language;
+                return marker;
+            }
+        };
+            
+        var getMarkersFromLanguages = function(languages) {
+            var markers = {};
+            for(var index = 0, nLanguages = languages.length; index < nLanguages; index++) {
+                var language = languages[index];
+                var location = language.location;
+                if(typeof language.name !== "undefined" && typeof location !== "undefined") {
+                    var languageCode = languageService.getLanguageCode(language);
+                    if(location instanceof Array) {
+                        for(var i = 0, n = location.length; i < n; i++) {
+                            var marker = getMarkerFromLanguage(language, location[i]);
+                            markers[languageCode + '_' + i] = marker;
+                        }
+                    }
+                    else {
+                        var marker = getMarkerFromLanguage(language);
+                        markers[languageCode] = marker;
+                    }
+                }
+            }
+            return markers;
+        };
+
+        var deferred = $q.defer();
+        
+        $http.get('data/languages.json')
+            .success(function(response) {
+                var markers = getMarkersFromLanguages(response);
+                deferred.resolve(markers);
+            })
+        ;
+
+        return {
+            getLanguageMarkers: function() {
+                return deferred.promise;
+            }
+        };
+
+    }])
+
+    .service('languageService', function() {
+
+        var visitorLanguage = "en";
+
+        return {
+            getLanguageCode: function(language) {
+                var languageCode;
+                if(language.codes) {
+                    languageCode = language.codes["639-3"] || language.codes["639-2"] || language.codes["639-1"];
+                    if(languageCode instanceof Array) {
+                        languageCode = languageCode[0];
+                    }
+                }
+                else {
+                    languageCode = language.code;
+                }
+                return languageCode;
+            },
+            getLanguageName: function(language) {
+                var languageName;
+                if(language.name[visitorLanguage] instanceof Array) {
+                    languageName = language.name[visitorLanguage][0];
+                }
+                else {
+                    if(typeof language.name[visitorLanguage] === 'string') {
+                        languageName = language.name[visitorLanguage];
+                    }
+                    else {
+                        languageName = language.name;
+                    }
+                }
+                return languageName;
+            }
+        };
+        
+    })
     
     ;
 
