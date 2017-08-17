61aead936017aa9ee499a627c2529dae41b35a69
Joan Perals <joan.perals@ixds.com>
2017-08-18 00:44:44 +0200

    Live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+const connect = require('gulp-connect');
 const del = require('del');
 const gulp = require('gulp');
 const yaml = require('gulp-yaml');
 
+const serverPort = 3333;
 const buildDir = './build';
 
 gulp.task('clean', function (callback) {
@@ -15,16 +17,30 @@ gulp.task('yaml', function () {
         .pipe(gulp.dest(buildDir));
 });
 
-gulp.task('bower', function() {
+gulp.task('bower', function () {
     gulp.src('./components/**')
         .pipe(gulp.dest(buildDir + '/components'));
 });
 
-gulp.task('src', function() {
+gulp.task('src', function () {
     gulp.src('./src/**')
         .pipe(gulp.dest(buildDir));
 });
 
+gulp.task('reload', function () {
+    gulp.src('./src/**/*')
+        .pipe(connect.reload());
+});
+
+gulp.task('serve', function () {
+    connect.server({
+        livereload: true,
+        port: serverPort,
+        root: [ buildDir ]
+    });
+    gulp.watch('src/**/*', ['src', 'yaml', 'reload']);
+});
+
 gulp.task('build', ['clean', 'src', 'yaml', 'bower']);
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'serve']);
